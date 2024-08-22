696cde72f799f3152f683a385095c17d98d17c8b
gaurav6386gaur <gauravsinghabc2@gmail.com>
2024-08-23 00:55:01 +0530

    Add files via upload

---
+++ accessing files using python.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+import requests
+from bs4 import BeautifulSoup
+url ='https://insights.blackcoffer.com/ml-and-ai-based-insurance-premium-model-to-predict-premium-to-be-charged-by-the-insurance-company/'
+requests.get(url)
